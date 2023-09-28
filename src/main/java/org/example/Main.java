62528db469a1a76582d2def255e9f55903e1fab2
Daniel Hall <snake05865@mail.ru>
2023-09-29 01:57:07 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,11 +33,7 @@ public class Main {
         static String calculate(String input) {
             String[] parts = input.split(" ");
             if (parts.length != 3) {
-                if (parts.length == 1) {
-                    throw new IllegalArgumentException("Строка не является математической операцией");
-                } else {
-                    throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, *, /)");
-                }
+                throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, *, /)");
             }
 
             String operand1 = parts[0];
@@ -47,26 +44,37 @@ public class Main {
                 throw new ArithmeticException("Деление на ноль");
             }
 
-            boolean isOperand1Roman = isRomanNumeral(operand1);
-            boolean isOperand2Roman = isRomanNumeral(operand2);
+            boolean isOperand1Roman = isValidRoman(operand1);
+            boolean isOperand2Roman = isValidRoman(operand2);
             boolean isOperand1Number = isNumber(operand1);
             boolean isOperand2Number = isNumber(operand2);
+
+            if ((!isOperand1Number && !isOperand1Roman) || (!isOperand2Number && !isOperand2Roman)) {
+                throw new IllegalArgumentException("Строка не является математической операцией");
+            }
 
             int a;
             int b;
 
             if (isOperand1Roman && isOperand2Roman) {
+                if (!isValidRoman(operand1) || !isValidRoman(operand2)) {
+                    throw new IllegalArgumentException("Операнды должны быть в диапазоне от I до X");
+                }
                 a = romanToArabic(operand1);
                 b = romanToArabic(operand2);
             } else if (isOperand1Number && isOperand2Number) {
                 a = Integer.parseInt(operand1);
                 b = Integer.parseInt(operand2);
             } else {
-                throw new IllegalArgumentException("Операнды должны быть либо оба арабскими числами, либо оба римскими");
+                throw new IllegalArgumentException("Используются одновременно разные системы счисления");
             }
 
             if (!isValidOperator(operator)) {
                 throw new IllegalArgumentException("Неподдерживаемая операция: " + operator);
+            }
+
+            if (operator.equals("-") && (isOperand1Roman || isOperand2Roman) && a < b) {
+                throw new IllegalArgumentException("В римской системе нет отрицательных чисел");
             }
 
             int result;
@@ -90,14 +98,9 @@ public class Main {
             return (isOperand1Roman || isOperand2Roman) ? arabicToRoman(result) : String.valueOf(result);
         }
 
-        static boolean isRomanNumeral(String input) {
-            String validRomanNumerals = "IVXLCD";
-            for (char c : input.toCharArray()) {
-                if (validRomanNumerals.indexOf(c) == -1) {
-                    return false;
-                }
-            }
-            return true;
+        static boolean isValidRoman(String input) {
+            String validRomanPattern = "^(I|II|III|IV|V|VI|VII|VIII|IX|X)$";
+            return Pattern.matches(validRomanPattern, input);
         }
 
         static int romanToArabic(String input) {
@@ -125,22 +128,22 @@ public class Main {
         }
 
         static String arabicToRoman(int num) {
-            if (num <= 0) {
-                throw new UnsupportedOperationException("В римской системе нет отрицательных чисел");
+            if (num < 1) {
+                throw new IllegalArgumentException("Римскими числа могут быть только положительные");
             }
 
-            String[] romanSymbols = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-            int[] arabicValues = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] romanSymbols = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+            int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
             StringBuilder result = new StringBuilder();
-            int i = 0;
 
-            while (num > 0) {
-                if (num >= arabicValues[i]) {
+            for (int i = values.length - 1; i >= 0; i--) {
+                while (num >= values[i]) {
                     result.append(romanSymbols[i]);
-                    num -= arabicValues[i];
-                } else {
-                    i++;
+                    num -= values[i];
                 }
+            }
+            if (result.length() == 0) {
+                throw new IllegalArgumentException("Результат операции не может быть представлен римскими цифрами");
             }
 
             return result.toString();
@@ -153,7 +156,10 @@ public class Main {
         static boolean isNumber(String input) {
             try {
                 int num = Integer.parseInt(input);
-                return num >= 1 && num <= 10;
+                if (num < 1 || num > 10) {
+                    throw new IllegalArgumentException("Операнды должны быть в диапазоне от 1 до 10");
+                }
+                return true;
             } catch (NumberFormatException e) {
                 return false;
             }
